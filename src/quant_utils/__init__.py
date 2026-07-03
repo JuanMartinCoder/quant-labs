84949e38422a9c62090951e45cb23ab76eca8362
JuanMartinCoder <skyzinformatica@gmail.com>
2026-07-03 22:47:50 +0000

    feat: Research-Backtesting 2024-2025

--- src/quant_utils/__init__.py
+++ src/quant_utils/__init__.py
@@ -2,5 +2,6 @@
 
 from .stats import FinancialStats
 from .optimization import PortfolioOptimizer
+from .plotting import QuantPlotter
 
 __all__ = ["FinancialStats", "PortfolioOptimizer", "QuantPlotter"]
